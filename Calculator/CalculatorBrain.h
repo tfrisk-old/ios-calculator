52b5fa002910252472d39f53a951032d32e8c23a
Teemu Frisk <teemufrisk@tfrisk-mbp.local>
2011-09-01 20:31:14 +0300

    Add CalculatorBrain class

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by Teemu Frisk on 1.9.2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+@end
---
+++ Calculator/CalculatorBrain.m
@@ -0,0 +1,23 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by Teemu Frisk on 1.9.2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
